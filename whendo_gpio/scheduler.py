a630cc71180d2df54289a119deb0cccce30f5d8b
electronhead <beaver@electronhead.com>
2021-03-27 21:23:42 -0700

    added description method to the gpio scheduler

--- whendo_gpio/scheduler.py
+++ whendo_gpio/scheduler.py
@@ -12,8 +12,14 @@ from whendo.core.executor import Executor
 
 
 class PinScheduler(ThresholdScheduler):
+    """
+    Detects a change to pin in one direction or both directions.
+    """
     pin: int  # synonymous with 'channel'
     event: int  # GPIO.FALLING, GPIO.RISING, GPIO.BOTH
+
+    def description(self):
+        return f"Executes a callback based on a supplied scheduler_name if a selected event has been detected in pin ({self.pin}). [GPIO.RISING ({GPIO.RISING}), GPIO.FALLING ({GPIO.FALLING}), GPIO.BOTH({GPIO.BOTH})] {super().description()}"
 
     def schedule(self, scheduler_name: str, executor: Executor):
         callable = self.during_period(scheduler_name=scheduler_name, executor=executor)
